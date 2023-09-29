8024154e056f762ee2cc9117b5700bfcb90b4e88
Adham <104841927+AdhamAbouaita@users.noreply.github.com>
2023-09-29 19:56:11 -0400

    Update iterative.cpp
    Added a more in depth explanation for the iterative complete search algorithm.

--- completesearch/iterative.cpp
+++ completesearch/iterative.cpp
@@ -6,14 +6,16 @@ int n;
 vector<int> v;
 
 void search() {
-    for (int b = 0; b < (1<<n); b++) {
+    // When you iterate up to 1 shifted left n times, you go through all the permutations of 1s and 0s.
+    // If we pretend that bits equal to 1 correspond to indices in the vector v, we can select every possible permutation/subset of the vector v.
+    for (int b = 0; b < (1<<n); b++) { 
         vector<int> subset; // this is where the current subset found is stored
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // Checks each bit in b, to see which ones are highlighted as one.
 
-            if (b&(1<<i)) subset.push_back(v[i]); // checks if that bit is 1.   
+            if (b&(1<<i)) subset.push_back(v[i]); // checks if that bit is 1. If it is, add the corresponding indice in "v" to the temp subset vector. 
         }
-        // print the subset:
+        // print the subset vector:
         for (auto it: subset) {
             cout << it << ' ';
         }
